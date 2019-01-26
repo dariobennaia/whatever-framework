ad841bab002034b81c040e1809bd88b8af9fd53f
Dario Santos <dariobennaia@gmail.com>
2019-01-26 15:52:04 -0200

    Criando rotas basicas de um CRUD

--- app/routes.php
+++ app/routes.php
@@ -6,9 +6,13 @@
  * Time: 21:01
  */
 
-$app->get('/', "App\Controllers\TestController::teste");
-$app->get('/all', "App\Controllers\TestController::index");
-$app->get('/show/{id}', "App\Controllers\TestController::show");
-$app->get('/create', "App\Controllers\TestController::store");
-$app->get('/update/{id}', "App\Controllers\TestController::update");
-$app->get('/delete/{id}', "App\Controllers\TestController::delete");
+$app->get('/', "App\Controllers\TestController::index");
+$app->get('/{id}', "App\Controllers\TestController::show");
+
+$app->get('/novo', "App\Controllers\TestController::create");
+$app->post('/store', "App\Controllers\TestController::store");
+
+$app->get('/atualizar/{id}', "App\Controllers\TestController::formAtualizar");
+$app->patch('/update/{id}', "App\Controllers\TestController::update");
+
+$app->delete('/delete/{id}', "App\Controllers\TestController::delete");
