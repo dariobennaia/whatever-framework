d6720923dcb6caf43af40ffe92e81476e0d46adf
Dario Santos <dariobennaia@gmail.com>
2019-01-26 16:02:00 -0200

    Reorganizando as rotas

--- app/routes.php
+++ app/routes.php
@@ -7,7 +7,6 @@
  */
 
 $app->get('/', "App\Controllers\TestController::index");
-$app->get('/{id}', "App\Controllers\TestController::show");
 
 $app->get('/novo', "App\Controllers\TestController::create");
 $app->post('/store', "App\Controllers\TestController::store");
@@ -16,3 +15,5 @@ $app->get('/atualizar/{id}', "App\Controllers\TestController::formAtualizar");
 $app->patch('/update/{id}', "App\Controllers\TestController::update");
 
 $app->delete('/delete/{id}', "App\Controllers\TestController::delete");
+
+$app->get('/{id}', "App\Controllers\TestController::show");
